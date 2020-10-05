557f610eb067f27a361a9644a1743cfee2b0dca3
Danielle MacDonald <spektre1@gmail.com>
2020-10-05 15:17:47 -0500

    kick script

--- setupWin.ps1
+++ setupWin.ps1
@@ -21,12 +21,15 @@ choco install -y --no-progress --force visualstudio2019community --package-param
 --add Microsoft.VisualStudio.Workload.Python"
 ```
 
+# Reset path after all the updates:
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
 # setup vcpkg:
 # https://github.com/microsoft/vcpkg
 $optDir = "C:\opt\"
-
-mkdir $optDir
+if (!(Test-Path $optDir)) { New-Item -ItemType 'directory' -path $optDir }
 set-location $optDir
+
 
 git clone git@github.com:microsoft/vcpkg.git
 .\vcpkg\bootstrap-vcpkg.bat
@@ -37,3 +40,71 @@ git clone git@github.com:microsoft/vcpkg.git
 $env:VCPKG_DEFAULT_TRIPLET = "x64-windows"
 .\vcpkg\vcpkg install boost:x64-windows
 
+
+# Manually update system path
+# This adds cmake
+$pathsToAdd = @('C:\Program Files\CMake\bin', 'D:\vcpkg\installed\x64-windows\tools\protobuf')
+
+# Does the whole system?
+# $regSysPath = 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment'
+# $oldSysPath = (Get-ItemProperty -Path $regSysPath -Name PATH).path
+# Set-ItemProperty -Path $regSysPath -Name PATH -Value ($oldSysPath + ';' + ($pathsToAdd -join ";"))
+
+$regUsrPath = 'Registry::HKEY_CURRENT_USER\Environment'
+$oldUsrPath = (Get-ItemProperty -Path $regUsrPath -Name PATH).path
+Set-ItemProperty -Path $regUsrPath -Name PATH -Value ($oldUsrPath + ';' + ($pathsToAdd -join ";"))
+
+# Manually adds to path
+$env:Path += ';' + ($pathsToAdd -join ";")
+
+
+# Automatically add the cmake toolchain file to my VS Code install
+$p = ($ENV:APPDATA + '\Code\User\settings.json')
+$s = Get-Content $p | ConvertFrom-Json -AsHashtable
+if (!($s.ContainsKey('cmake.configureSettings'))) {
+    $cmOptDir = $optDir.Replace('\', '/') + 'vcpkg'
+    $s.'cmake.configureSettings' = @{CMAKE_TOOLCHAIN_FILE = "$cmOptDir/scripts/buildsystems/vcpkg.cmake"}
+    $s | ConvertTo-Json | Out-File $p
+}
+
+
+# Install my rec'd VSCode extensions
+$vsCodeExtToInstall = @("aaron-bond.better-comments",
+"budparr.language-hugo-vscode",
+"CoenraadS.bracket-pair-colorizer",
+"cymonk.sql-formatter",
+"DavidAnson.vscode-markdownlint",
+"dbaeumer.vscode-eslint",
+"eamodio.gitlens",
+"esbenp.prettier-vscode",
+"lihui.vs-color-picker",
+"marcostazi.VS-code-vagrantfile",
+"ms-python.python",
+"ms-vscode.cmake-tools",
+"ms-vscode.cpptools",
+"ms-vscode.powershell",
+"ms-vsliveshare.vsliveshare",
+"msjsdiag.debugger-for-chrome",
+"naumovs.color-highlight",
+"oderwat.indent-rainbow",
+"Tobiah.language-pde",
+"twxs.cmake",
+"VisualStudioExptTeam.vscodeintellicode",
+"vsciot-vscode.vscode-arduino",
+"zxh404.vscode-proto3")
+
+forEach ($ext in $vsCodeExtToInstall) {
+    code --install-extension $ext
+}
+
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+Install-Module VSSetup
+
+Write-Host -ForegroundColor Green @"
+
+
+================================================================================
+! System should be fully kicked! GLHF
+================================================================================
+
+"@
